18966779383503ea7e8846db9d96d202e3b88f41
Temime Hammami <Temime.Hammami@businessdecision.com>
2017-04-25 18:43:47 +0100

    second commit

--- src/main/java/com/businessdecision/ocp/OMS.java
+++ src/main/java/com/businessdecision/ocp/OMS.java
@@ -17,12 +17,6 @@ import org.apache.spark.streaming.api.java.*;
 import scala.Tuple2;
 
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.sql.DataFrame;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SQLContext;
-
-import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.streaming.kafka.KafkaUtils;
 import kafka.serializer.StringDecoder;
@@ -31,18 +25,12 @@ import com.mysql.jdbc.*;
 /**
  * Created by Moncef.Bettaieb on 19/04/2017.
  */
-public final class  OMS {
-    private static final org.apache.log4j.Logger LOGGER = org.apache.log4j.Logger.getLogger(OMS.class);
+public final class OMS {
 
     public static final String DRIVER = "com.mysql.jdbc.Driver";
     public static final String URL = "jdbc:mysql://10.21.62.49:3306/ocp_maint";
     public static final String USERNAME = "root";
     public static final String PASSWORD = "SPLXP026";
-
-    private static final JavaSparkContext sc =
-            new JavaSparkContext(new SparkConf().setAppName("SparkJdbcDs").setMaster("local[*]"));
-
-    private static final SQLContext sqlContext = new SQLContext(sc);
 
     private OMS() {
     }
@@ -55,95 +43,89 @@ public final class  OMS {
             System.exit(1);
         }
 
+
+        JavaSparkContext sc =
+                new JavaSparkContext(new SparkConf().setAppName("Spark Example"));
+        SQLContext sqlContext = new SQLContext(sc);
         Class.forName("com.mysql.jdbc.Driver");
 
         Map<String, String> options = new HashMap<String, String>();
         options.put("driver", DRIVER);
-        options.put("url", URL);
-        options.put("dbtable",
-                "(select idAlert, concat_ws(' ', idAlert, idPom) as full_name from Alert) as Alert_name");
-        options.put("partitionColumn", "idAlert");
-        options.put("lowerBound", "10001");
-        options.put("upperBound", "499999");
-        options.put("numPartitions", "10");
+        options.put("url", URL + "?user=" + USERNAME + "&password=" + PASSWORD);
+        options.put("dbtable", "Alert");
 
+        final DataFrame df = sqlContext
+                .read()
+                .format("jdbc")
+                .options(options)
+                .load();
 
-        //Load MySQL query result as DataFrame
-        DataFrame jdbcDF = sqlContext.load("jdbc", options);
+// Looks the schema of this DataFrame.
+        df.printSchema();
+        df.show();
 
+        String brokers = args[0];
+        String topics = args[1];
 
+        //SparkConf sparkConf = new SparkConf().setAppName("OMS Maintenance Spark Streaming");
+        JavaStreamingContext jssc = new JavaStreamingContext(sc, Durations.seconds(5));
 
+        HashSet<String> topicsSet = new HashSet<String>(Arrays.asList(topics.split(",")));
+        HashMap<String, String> kafkaParams = new HashMap<String, String>();
+        kafkaParams.put("metadata.broker.list", brokers);
 
+        JavaPairInputDStream<String, String> messages = KafkaUtils.createDirectStream(
+                jssc,
+                String.class,
+                String.class,
+                StringDecoder.class,
+                StringDecoder.class,
+                kafkaParams,
+                topicsSet
+        );
 
-        List<Row> idAlertRows = jdbcDF.collectAsList();
-
-            for (Row idAlertRow : idAlertRows) {
-                LOGGER.info(idAlertRow);
-
-
-            }
-
-                    String brokers = args[0];
-            String topics = args[1];
-
-            //SparkConf sparkConf = new SparkConf().setAppName("OMS Maintenance Spark Streaming");
-            JavaStreamingContext jssc = new JavaStreamingContext(sc, Durations.seconds(5));
-
-            HashSet<String> topicsSet = new HashSet<String>(Arrays.asList(topics.split(",")));
-            HashMap<String, String> kafkaParams = new HashMap<String, String>();
-            kafkaParams.put("metadata.broker.list", brokers);
-
-            JavaPairInputDStream<String, String> messages = KafkaUtils.createDirectStream(
-                    jssc,
-                    String.class,
-                    String.class,
-                    StringDecoder.class,
-                    StringDecoder.class,
-                    kafkaParams,
-                    topicsSet
-            );
-
-            JavaDStream<String> lines = messages.map(new Function<Tuple2<String, String>, String>() {
-                public String call(Tuple2<String, String> tuple2) {
-
-                    try {
-                        JSONObject obj = new JSONObject(tuple2._2());
-                        String status = obj.getString("status");
-                        String date = obj.getJSONObject("end").getString("$date");
-                        String gpk = "";
-                        String rms = "";
-                        JSONArray arr = obj.getJSONArray("globals");
-                        gpk = arr.getString(0);
-                        rms = arr.getString(1);
-                        String pom = obj.getJSONObject("pom").getString("$oid");
-                        String extTemp = obj.getJSONObject("values").getString("exttemp");
-                        String taskId = obj.getJSONObject("taskid").getString("$oid");
-                        String factor = obj.getString("factor");
-                        String result = status + "," + date + "," + gpk + "," + rms + "," + pom + "," + extTemp + "," + taskId + "," + factor;
-                        Properties props = new Properties();
-                        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "10.21.62.48:9092");
-                        props.put(ProducerConfig.RETRIES_CONFIG, "3");
-                        props.put(ProducerConfig.ACKS_CONFIG, "all");
-                        props.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "none");
-                        props.put(ProducerConfig.BATCH_SIZE_CONFIG, 200);
-                        props.put(ProducerConfig.BLOCK_ON_BUFFER_FULL_CONFIG, true);
-                        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
-                        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
-                        KafkaProducer producer = new KafkaProducer<String, String>(props);
-                        producer.send(new ProducerRecord<String, String>("events",
-                                result));
-                        return result;
-                    } catch (JSONException e) {
-                        return "Json Exception : " + e;
-                    }
+        JavaDStream<String> lines = messages.map(new Function<Tuple2<String, String>, String>() {
+            public String call(Tuple2<String, String> tuple2) {
+                df.show();
+                try {
+                    JSONObject obj = new JSONObject(tuple2._2());
+                    String status = obj.getString("status");
+                    String date = obj.getJSONObject("end").getString("$date");
+                    String gpk = "";
+                    String rms = "";
+                    JSONArray arr = obj.getJSONArray("globals");
+                    gpk = arr.getString(0);
+                    rms = arr.getString(1);
+                    String pom = obj.getJSONObject("pom").getString("$oid");
+                    String extTemp = obj.getJSONObject("values").getString("exttemp");
+                    String taskId = obj.getJSONObject("taskid").getString("$oid");
+                    String factor = obj.getString("factor");
+                    String result = status+","+date+","+gpk+","+rms+","+pom+","+extTemp+","+taskId+","+factor;
+                    Properties props = new Properties();
+                    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "10.21.62.48:9092");
+                    props.put(ProducerConfig.RETRIES_CONFIG, "3");
+                    props.put(ProducerConfig.ACKS_CONFIG, "all");
+                    props.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "none");
+                    props.put(ProducerConfig.BATCH_SIZE_CONFIG, 200);
+                    props.put(ProducerConfig.BLOCK_ON_BUFFER_FULL_CONFIG, true);
+                    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+                    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+                    KafkaProducer producer = new KafkaProducer<String,String>(props);
+                    producer.send(new ProducerRecord<String, String>("events",
+                            result));
+                    return result;
                 }
-            });
-            lines.print();
-        jdbcDF.show();
-            //JavaRDD mysql = new JdbcRDD<String>();
+                catch (JSONException e){
+                    return "Json Exception : "+e ;
+                }
+            }
+        });
+        lines.print();
 
-            jssc.start();
-            jssc.awaitTermination();
-        }
+        //JavaRDD mysql = new JdbcRDD<String>();
 
+        jssc.start();
+        jssc.awaitTermination();
     }
+
+}
