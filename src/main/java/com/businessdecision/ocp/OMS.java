dbd50d3aab2d0595d374dbb678ab5e6bbe7077e9
Hammami Temime <temime.hammami@gmail.com>
2017-04-25 17:43:23 +0200

    Update OMS.java

--- src/main/java/com/businessdecision/ocp/OMS.java
+++ src/main/java/com/businessdecision/ocp/OMS.java
@@ -63,7 +63,8 @@ public final class OMS {
 // Looks the schema of this DataFrame.
         df.cache();
         df.show();
-
+ df.cache();
+        df.show();
         String brokers = args[0];
         String topics = args[1];
 
